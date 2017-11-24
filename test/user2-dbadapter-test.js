885ea05edf5e63dbe3f86db5ec0a99dce09d0509
min_kinggod <heeeee123@ajou.ac.kr>
2017-11-24 21:37:30 +0900

    user2-db연결했음

--- test/user2-dbadapter-test.js
+++ test/user2-dbadapter-test.js
@@ -1,6 +1,6 @@
 var user2db = require('../adapters/user2db-adapter');
 
-user2db.search(1,[],function (result,rows) {
+user2db.search('ok',[],function (result,rows) {
     for ( i in rows){
         console.log(rows[i])
     }
