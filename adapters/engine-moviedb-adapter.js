b85ed3ed16d1368a838e86e1474f8b9adf4e235f
dogfooter jason <dogfooter@dogfooterui-MacBook-Pro.local>
2017-11-25 16:46:57 +0900

    engine-movie skeleton

---
+++ adapters/engine-moviedb-adapter.js
@@ -0,0 +1,7 @@
+const dbResultCode = require('../status-codes/db-result');
+
+var pool = require('./mysql-pool');
+
+var adapter = {};
+
+module.exports = adapter;
---
+++ models/engine-movie-actor.js
@@ -0,0 +1,5 @@
+class EngineMoive {
+
+}
+
+module.exports = EngineMoive;
