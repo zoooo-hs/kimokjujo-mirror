decb49ea09dd311213e8cb1ec240d03fbd209bb1
HyeonsuJu <dogfooter219@gmail.com>
2017-12-02 18:19:10 +0900

    Revert "engine server connected"
    This reverts commit 5ce8664f9173e0bf27ef4c64167fe825e8682c2e.

--- adapters/engine-adapter.js
+++ adapters/engine-adapter.js
@@ -1,23 +1,13 @@
-var request = require('request');
-var uniquid = require('uniquid');
-
 var adapter = {};
 
 adapter.runEngine = function (planMovieId, actor1Id, actor2Id, cb) {
 
-    var uid = uniquid();
-    console.log(uid);
-    request('http://localhost:10001/?uid='+uid+'&planMovieId='+planMovieId+'&actor1Id='+actor1Id+'&actor2Id='+actor2Id, function(err, response, body) {
-        var data = JSON.parse(body);
-        console.log(data)
-        if (data.err) {
-            console.log(err);
-            cb(data.err, []);
-        }
-        else {
-            cb(data.err, [parseInt(data.result.regression), parseInt(data.result.naive)]);
-        }
-    }); 
+    var regression = 10000;
+    var naive = 0;
+
+    var err = 0;
+
+    cb(err, [regression, naive]);
 };
 
 module.exports = adapter;
