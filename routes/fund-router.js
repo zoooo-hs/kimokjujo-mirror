875b872705898a4d36eedd7c9af5a91b4474304b
HyeonsuJu <dogfooter219@gmail.com>
2017-12-07 11:07:04 +0900

    fund list

--- routes/fund-router.js
+++ routes/fund-router.js
@@ -53,95 +53,29 @@ router.get('/', function(req, res, next) {
         }
     });
 });
-
-router.get('/:planMovieId', function (req, res, next) {
-
-    var sessionKey = req.cookies.sessionkey;
-
-    var returnJSON = { success: false };
-
-    var userType = req.cookies.userType;
-
-    var userCheck = false;
-
-    var inputPlanMovieId = req.params.planMovieId;
-
-    sessionAdapter.search(sessionKey, null, function (resultCode, rows) {
-
-        if (resultCode == dbResultCode.OK) {
-            if (rows.length != 0) {
-
-                var userId = rows[0].userId;
-
-                var result = {};
-
-                planMovieAdapter.searchByUserId(userId, null, function (resultCode, rows) {
-                    if (resultCode == dbResultCode.OK) {
-                        for (var i in rows) {
-                            var row = rows[i];
-                            var planMovie = {};
-                            var actors = row.actorIds.split(',');
-                            planMovie = row;
-                            delete row.actorIds;
-                            if (inputPlanMovieId == planMovie.id) {
-                                console.log(planMovie.id);
-                                userCheck = true;
-                                break;
-                            }
-                            else {
-                                continue;
-                            }
-                        }
-                        if (userCheck == true) {
-                            resultMovieAdapter.searchByPlanMovieId(planMovie.id, [], function (resultCode, rows) {
-                                if (resultCode = dbResultCode.OK) {
-                                    var resultMovie = rows[0];
-                                    returnJSON.success = true;
-                                    returnJSON.planMovie = {
-                                        id: planMovie.id,
-                                        title: planMovie.title,
-                                        original: planMovie.original,
-                                        _3words: planMovie._3words,
-                                        budget: planMovie.budget,
-                                        releaseMonth: planMovie.releaseMonth,
-                                        contentRate: planMovie.contentRate,
-                                        directorId: planMovie.directorId,
-                                        makerId: planMovie.makerId,
-                                        genre: planMovie.genre
-                                    };
-                                    returnJSON.actors = actors;
-                                    returnJSON.planMovieResult = {
-                                        date: resultMovie.date,
-                                        scenario: resultMovie.scenario,
-                                        audience: resultMovie.audience,
-                                        breakEvenPoint: resultMovie.breakEvenPoint,
-                                        contract: resultMovie.contract
-                                    };
-                                    res.json(returnJSON);
-                                } // resultcode ok
-                                else {
-                                    res.json(returnJSON); 
-                                }
-                            });
-                        }
-                        else {
-                            res.json(returnJSON);
-                        }
-                    }
-                    else {
-                        res.json(returnJSON);
-                    }
-                });
+router.get('/:planMovieId',function(req, res, next){
+    
+        console.log(req.cookies);
+    
+        var sessionKey = req.cookies.sessionkey;
+    
+        sessionAdapter.search(sessionKey, null, function (resultCode, rows) {
+    
+            if (resultCode == dbResultCode.OK) {
+                if (rows.length != 0) {
+                    var userId = rows[0].userId;
+                    console.log(userId);
+    
+                    sendHtml.sendHTML('fund-info', res, next); 
+                }
+                else {
+                    next();
+                }
             }
             else {
-                res.json(returnJSON);
+                next();
             }
-        }
-        else {
-            res.json(returnJSON);
-        }
+        });
+    
     });
-
-});
-
 module.exports = router;
