c9a00303af9898a53d5764fa34cc865a6715c167
min_kinggod <heeeee123@ajou.ac.kr>
2017-12-10 16:03:17 +0900

    contract 수정 및 router 추가

---
+++ routes/contract-router.js
@@ -0,0 +1,28 @@
+var router = require('express').Router();
+var contractAdapter = require('../adapters/contract-adapter');
+
+const dbResultCode = require('../status-codes/db-result');
+
+router.post('/', function (req, res, next) {
+
+    var userId = req.cookies.userId;
+    var planMovieId = req.cookies.planMovieId;
+
+    if (req.cookies.userId == undefined || req.body.planMovieId == undefined) {
+        return res.json({success: false});
+    }
+
+    else {
+        contractAdapter.updateUserId(planMovieId, userId, function (resultCode) {
+            if(resultCode == dbResultCode.Fail){
+                res.json({success: false});
+            }
+            else{
+                //메일을 보낸다!  
+                res,json({success: true});
+            }
+        });
+    }
+});
+
+module.exports = router;
